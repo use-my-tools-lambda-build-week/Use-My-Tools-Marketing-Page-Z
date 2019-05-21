56b824af9697be4026bd06381207016dea8fdac5
Vaeliwen <zaman@imbriaco.com>
2019-05-21 16:15:24 -0400

    Completed JavaScript functionality for links.

---
+++ index.js
@@ -0,0 +1,24 @@
+class PageLink {
+    constructor(pageElement) {
+        this.pageElement = pageElement;
+        this.pageData = this.pageElement[`data-page='${this.pageElement.dataset.page}'`]
+        this.pageContent = document.querySelector(`.content[data-page='${this.pageElement.dataset.page}']`)
+        this.pageElement.addEventListener('click', () => this.selectPage())
+    }
+    selectPage() {
+        const pages = document.querySelectorAll('.navbar')
+        for (let i = 0; i < pages.length; i++) {
+            pages[i].classList.remove('active-page')
+        }
+        const contents = document.querySelectorAll('.content')
+        for (let i = 0; i < contents.length; i++) {
+            contents[i].style.display = 'none';
+        }
+        this.pageElement.classList.add('active-page')
+        this.pageContent.style.display = 'flex';
+    }
+}
+
+let links = document.querySelectorAll('.navbar')
+
+links.forEach(link => new PageLink(link))
